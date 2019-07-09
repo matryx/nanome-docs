4b35653c786a8ce82593e5703e5c52c1b65e598d
Austin Gil <austin@stegosource.com>
2019-07-09 16:05:29 -0700

    Cleaning up content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,27 +1,30 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-var dirpath = "./docs"
+var dirpath = "./docs";
 
 // generate sidenav order by update time
-var sb = fs.readdirSync(dirpath).filter(f => {
-    return f.match(/\.(md)$/i) && f !== 'README.md'
-}).map(f => {
-  return {
-    path:  '/' + f,
-    mtime: fs.statSync(dirpath +  '/' + f).mtime
-  }
-})
-.sort((a, b) => a.mtime - b.mtime)
-.map(f => f.path)
+var sb = fs
+  .readdirSync(dirpath)
+  .filter(f => {
+    return f.match(/\.(md)$/i) && f !== "README.md";
+  })
+  .map(f => {
+    return {
+      path: "/" + f,
+      mtime: fs.statSync(dirpath + "/" + f).mtime
+    };
+  })
+  .sort((a, b) => a.mtime - b.mtime)
+  .map(f => f.path);
 
 module.exports = {
-    title: 'Netlify CMS + VuePress',
-    description: 'Netlify + VuePress',
-    themeConfig: {
-      docsDir: 'docs',
-      repo: 'toshilow/netlify-cms-vuepress',
-      sidebar: sb,
-      lastUpdated: 'Last Updated'
-    }
+  title: "Nanome",
+  description: "Documentation site for Nanome software.",
+  themeConfig: {
+    docsDir: "docs",
+    // repo: 'toshilow/netlify-cms-vuepress',
+    sidebar: sb,
+    lastUpdated: "Last Updated"
   }
+};
