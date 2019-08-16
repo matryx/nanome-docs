e83b71f9f0d7b706776347ed35abc477351223a5
Alex McNerney <ajm13@me.com>
2019-08-16 14:49:48 -0700

    Updated config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-
-var dirpath = "./docs";
-
-// generate sidenav order by update time
-// var sb = fs
-//   .readdirSync(dirpath)
-//   .filter(f => {
-//     return f.match(/\.(md)$/i) && f !== "README.md";
-//   })
-//   .map(f => {
-//     return {
-//       path: "/" + f,
-//       mtime: fs.statSync(dirpath + "/" + f).mtime
-//     };
-//   })
-//   .sort((a, b) => a.mtime - b.mtime)
-//   .map(f => f.path);
-
 module.exports = {
-  description: "Documentation site for Nanome software.",
+  description: 'Documentation site for Nanome software.',
   themeConfig: {
-    home:true,
-    title: "Documentation",
-    docsDir: "docs",
-    logo: '../assets//features-page/nanome-logo.png',
-    // repo: 'toshilow/netlify-cms-vuepress',
+    home: true,
+    title: 'Documentation',
+    docsDir: 'docs',
+    logo: '../assets/features-page/nanome-logo.png',
     sidebar: [
       '/',
       '/gettingStarted',
@@ -35,13 +14,12 @@ module.exports = {
       '/troubleshooting',
       '/contact'
     ],
-    lastUpdated: "Last Updated",
+    lastUpdated: 'Last Updated',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Nanome.ai', link: 'https://nanome.ai/' },
-      { text: 'Download', link: 'https://nanome.ai/setup' },
+      { text: 'Download', link: 'https://home.nanome.ai/setup' }
     ],
     ga: 'UA-79179574-15'
   }
-};
-
+}
