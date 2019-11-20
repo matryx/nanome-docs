7d5c9b221c1f036d00efd94aaa88ed6415827fd0
Alex McNerney <ajm13@me.com>
2019-11-19 16:01:09 -0800

    config cleanup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  description: "Documentation site for Nanome software.",
+  description: 'Documentation site for Nanome software.',
   themeConfig: {
     home: true,
-    title: "Documentation",
-    docsDir: "docs",
-    logo: "../assets/features-page/nanome-logo.png",
+    title: 'Documentation',
+    docsDir: 'docs',
+    logo: '../assets/features-page/nanome-logo.png',
     sidebar: [
-      "/",
+      '/',
       {
-        title: 'Nanome Navigation',   // required
-        path: '/navigation/',  // optional, which should be a absolute path.
-        collapsable: true,     // optional, defaults to true
-        sidebarDepth: 1,       // optional, defaults to 1
+        title: 'Nanome Navigation',
+        path: '/navigation/',
+        collapsable: true,
+        sidebarDepth: 1,
         children: [
           '/navigation/lobby',
           '/navigation/environment',
@@ -19,14 +19,14 @@ module.exports = {
           '/navigation/wristmenu',
           '/navigation/contextmenu',
           '/navigation/toolsmenu',
-          '/navigation/2dui',
+          '/navigation/2dui'
         ]
       },
       {
-        title: 'Nanome Plugins',   // required
-        path: '/plugins/',  // optional, which should be a absolute path.
-        collapsable: true,     // optional, defaults to true
-        sidebarDepth: 1,       // optional, defaults to 1
+        title: 'Nanome Plugins',
+        path: '/plugins/',
+        collapsable: true,
+        sidebarDepth: 1,
         children: [
           '/plugins/overview',
           '/plugins/nanomeplugins',
@@ -40,39 +40,35 @@ module.exports = {
         ]
       },
       {
-        title: 'Nanome Macros',   // required
-        path: '/macros/',         // optional, which should be a absolute path.
-        collapsable: true,        // optional, defaults to true
-        sidebarDepth: 1,          // optional, defaults to 1
-        children: [
-          '/macros/overview'
-        ]
+        title: 'Nanome Macros',
+        path: '/macros/',
+        collapsable: true,
+        sidebarDepth: 1,
+        children: ['/macros/overview']
       },
       {
-        title: 'How To (Coming Soon...)',   // required
-        path: '/howto/',  // optional, which should be a absolute path.
-        collapsable: true,     // optional, defaults to true
-        sidebarDepth: 1,       // optional, defaults to 1
-        children: [
-          '/howto/comingsoon'
-        ]
+        title: 'How To (Coming Soon...)',
+        path: '/howto/',
+        collapsable: true,
+        sidebarDepth: 1,
+        children: ['/howto/comingsoon']
       },
       {
-        title: 'home.nanome.ai',   // required
-        path: '/home.nanome.ai/',  // optional, which should be a absolute path.
-        collapsable: true,     // optional, defaults to true
-        sidebarDepth: 1,       // optional, defaults to 1
+        title: 'home.nanome.ai',
+        path: '/home.nanome.ai/',
+        collapsable: true,
+        sidebarDepth: 1,
         children: [
           '/home.nanome.ai/overview',
-          "/home.nanome.ai/accounttiers",
+          '/home.nanome.ai/accounttiers',
           '/home.nanome.ai/invoice'
         ]
       },
       {
-        title: 'Help',   // required
-        path: '/help/',  // optional, which should be a absolute path.
-        collapsable: true,     // optional, defaults to true
-        sidebarDepth: 1,       // optional, defaults to 1
+        title: 'Help',
+        path: '/help/',
+        collapsable: true,
+        sidebarDepth: 1,
         children: [
           // '/help/howto',
           '/help/troubleshooting',
@@ -83,12 +79,12 @@ module.exports = {
       }
     ],
     sidebarDepth: 2,
-    lastUpdated: "Last Updated",
+    lastUpdated: 'Last Updated',
     nav: [
-      { text: "Home", link: "/" },
-      { text: "Nanome.ai", link: "https://nanome.ai/" },
-      { text: "Download", link: "https://home.nanome.ai/setup" }
+      { text: 'Home', link: '/' },
+      { text: 'Nanome.ai', link: 'https://nanome.ai/' },
+      { text: 'Download', link: 'https://home.nanome.ai/setup' }
     ],
-    ga: "UA-79179574-15"
+    ga: 'UA-79179574-15'
   }
 }
