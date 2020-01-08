89d462a87663653ee2524600160d52982d3fd11e
Tryston Perry <Tryston@TrystonPerry.com>
2020-01-08 11:08:24 -0800

    Updated image compression to be recursive

--- docs/.vuepress/compress.js
+++ docs/.vuepress/compress.js
@@ -1,4 +1,6 @@
 const sharp = require("sharp");
+const imagemin = require("imagemin");
+const imageminGifsicle = require("imagemin-gifsicle");
 const fs = require("fs");
 
 const assetDir = __dirname + "/public/assets/original";
@@ -8,70 +10,88 @@ const regex = {
   file: /^(.+)\.(.+)$/
 };
 
-(async () => {
-  console.log("Compressing images");
+(async function() {
+  console.log("Compressing images and gifs");
+  await recursiveCompress("");
+  console.log("Done compressing");
+})();
 
-  // Get all dirs
-  const dirs = await getDirs(assetDir);
+async function recursiveCompress(dir) {
+  await compressDir(dir);
 
-  // Loop through all dirs
-  dirs.forEach(async dir => {
-    const files = await getFiles(`${assetDir}/${dir}`);
+  const dirs = await getDirs(`${assetDir}/${dir}`);
 
-    if (!fs.existsSync(`${compressedDir}/${dir}`)) {
-      fs.mkdirSync(`${compressedDir}/${dir}`);
-    }
+  if (!fs.existsSync(compressedDir)) {
+    fs.mkdirSync(compressedDir);
+  }
 
-    // Loop through all files in dir
-    files.forEach(async file => {
-      const match = file.match(regex.file);
-      const fileName = match[1];
-      const format = match[2];
+  for (let i = 0; i < dirs.length; i++) {
+    await recursiveCompress(`${dir}/${dirs[i]}`);
+  }
+}
 
-      if (format !== "gif") {
-        let image = await sharp(`${assetDir}/${dir}/${file}`);
-        const data = await image.metadata();
+async function compressDir(dir) {
+  // Check if directory exists in compressed folder. If not, make it
+  if (!fs.existsSync(`${compressedDir}/${dir}`)) {
+    fs.mkdirSync(`${compressedDir}/${dir}`);
+  }
 
-        // If needs to be compressed
-        let { width, height } = data;
-        if (width > 450) {
-          const ratio = 450 / width;
-          width *= ratio;
-          height *= ratio;
-        }
-        if (height > 450) {
-          const ratio = 450 / height;
-          width *= ratio;
-          height *= ratio;
-        }
+  if (dir) {
+    console.log(`Compressing all images and gifs in ${dir}`);
+  }
 
-        // Convert from float to int
-        width = parseInt(width);
-        height = parseInt(height);
-
-        // If needs resizing
-        if (width < data.width || height < data.height) {
-          image = await image.resize(width, height);
-
-          // console.log(
-          //   `Compressing: "/${dir}/${file}" (${data.width}x${data.height}) -> "/${dir}/${fileName}" (${width}x${height})`
-          // );
-        }
-
-        await image
-          .toFormat("jpeg")
-          .toFile(`${compressedDir}/${dir}/${fileName}.jpg`);
-      } else {
-        await fs.copyFileSync(
-          `${assetDir}/${dir}/${fileName}.gif`,
-          `${compressedDir}/${dir}/${fileName}.gif`
-        );
-      }
-    });
+  // Compress gifs
+  await imagemin([`docs/.vuepress/public/assets/original/${dir}/*.gif`], {
+    destination: `docs/.vuepress/public/assets/compressed/${dir}`,
+    plugins: [
+      imageminGifsicle({
+        optimizationLevel: 3,
+        colors: 128
+      })
+    ]
   });
 
-  console.log("Done compressing images!");
-})();
+  const files = await getFiles(`${assetDir}/${dir}`);
+
+  // Loop through all files in dir
+  for (let i = 0; i < files.length; i++) {
+    const match = files[i].match(regex.file);
+    const fileName = match[1];
+    const format = match[2];
+
+    // If file type is a match
+    if (/^(png|jpg|jpeg|)$/.test(format.toLowerCase())) {
+      let image = await sharp(`${assetDir}/${dir}/${files[i]}`);
+      const data = await image.metadata();
+
+      // If needs to be compressed
+      let { width, height } = data;
+      if (width > 450) {
+        const ratio = 450 / width;
+        width *= ratio;
+        height *= ratio;
+      }
+      if (height > 450) {
+        const ratio = 450 / height;
+        width *= ratio;
+        height *= ratio;
+      }
+
+      // Convert from float to int
+      width = parseInt(width);
+      height = parseInt(height);
+
+      // If needs resizing
+      if (width < data.width || height < data.height) {
+        image = await image.resize(width, height);
+      }
+
+      await image
+        .toFormat("jpeg")
+        .toFile(`${compressedDir}/${dir}/${fileName}.jpg`);
+    }
+  }
+}
 
 function getDirs(dirname) {
   return fs
