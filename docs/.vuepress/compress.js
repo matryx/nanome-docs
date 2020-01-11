b19f6a6f17cdefca878347803fc13e732ae2e0f6
Alex McNerney <ajm13@me.com>
2020-01-10 16:41:37 -0800

    Minor cleanup on compress.js

--- docs/.vuepress/compress.js
+++ docs/.vuepress/compress.js
@@ -1,83 +1,82 @@
-const sharp = require('sharp');
+const sharp = require('sharp')
 // const imagemin = require('imagemin');
 // const imageminGifsicle = require('imagemin-gifsicle');
-const fs = require('fs');
+const fs = require('fs')
 
-const assetDir = __dirname + '/public/assets/original';
-const compressedDir = __dirname + '/public/assets/compressed';
+const assetDir = __dirname + '/public/assets/original'
+const compressedDir = __dirname + '/public/assets/compressed'
 
 const regex = {
-  file: /^(.+)\.(.+)$/
-};
+  file: /^(.+)\.(\w+)$/
+}
 
-(async function() {
-  console.log('Compressing images and gifs');
-  await recursiveCompress('');
-  console.log('Done compressing');
-})();
+;(async function() {
+  console.log('Compressing images and gifs')
+  await recursiveCompress('')
+  console.log('Done compressing')
+})()
 
-async function recursiveCompress(dir) {
-  await compressDir(dir);
+async function recursiveCompress(parent) {
+  await compressDir(parent)
 
-  const dirs = await getDirs(`${assetDir}/${dir}`);
+  const dirs = await getDirs(`${assetDir}/${parent}`)
 
   if (!fs.existsSync(compressedDir)) {
-    fs.mkdirSync(compressedDir);
+    fs.mkdirSync(compressedDir)
   }
 
-  for (let i = 0; i < dirs.length; i++) {
-    await recursiveCompress(`${dir}/${dirs[i]}`);
+  for (const dir of dirs) {
+    await recursiveCompress(`${parent}/${dir}`)
   }
 }
 
 async function compressDir(dir) {
   // Check if directory exists in compressed folder. If not, make it
   if (!fs.existsSync(`${compressedDir}/${dir}`)) {
-    fs.mkdirSync(`${compressedDir}/${dir}`);
+    fs.mkdirSync(`${compressedDir}/${dir}`)
   }
 
-  const files = await getFiles(`${assetDir}/${dir}`);
+  const files = await getFiles(`${assetDir}/${dir}`)
 
   // Loop through all files in dir
-  for (let i = 0; i < files.length; i++) {
-    const match = files[i].match(regex.file);
-    const fileName = match[1];
-    const format = match[2];
+  for (const file of files) {
+    const match = file.match(regex.file)
+    const [, fileName, format] = match
 
     // If file type is a match
     if (/png|jpg|jpeg|gif/i.test(format)) {
       if (fs.existsSync(`${compressedDir}${dir}/${fileName}.jpg`)) {
-        continue;
+        continue
       }
 
-      let image = await sharp(`${assetDir}/${dir}/${files[i]}`);
-      const data = await image.metadata();
+      let image = await sharp(`${assetDir}/${dir}/${file}`)
+      const data = await image.metadata()
 
       // If needs to be compressed
-      let { width, height } = data;
+      let { width, height } = data
       if (width > 450) {
-        const ratio = 450 / width;
-        width *= ratio;
-        height *= ratio;
+        const ratio = 450 / width
+        width *= ratio
+        height *= ratio
       }
       if (height > 450) {
-        const ratio = 450 / height;
-        width *= ratio;
-        height *= ratio;
+        const ratio = 450 / height
+        width *= ratio
+        height *= ratio
       }
 
       // Convert from float to int
-      width = parseInt(width);
-      height = parseInt(height);
+      width = parseInt(width)
+      height = parseInt(height)
 
       // If needs resizing
       if (width < data.width || height < data.height) {
-        image = await image.resize(width, height);
+        image = await image.resize(width, height)
       }
 
       await image
         .toFormat('jpeg')
-        .toFile(`${compressedDir}/${dir}/${fileName}.jpg`);
+        .toFile(`${compressedDir}/${dir}/${fileName}.jpg`)
     }
 
     // If file type is gif, compress
@@ -100,7 +99,7 @@ async function compressDir(dir) {
     //   );
     // }
 
-    console.log(`- Compressed ${dir}/${files[i]}`);
+    console.log(`- Compressed ${dir}/${file}`)
   }
 }
 
@@ -110,8 +109,8 @@ function getDirs(dirname) {
     .readdirSync(dirname, {
       withFileTypes: true
     })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
+    .filter(item => item.isDirectory())
+    .map(item => item.name)
 }
 
 // Get all files by directory name
@@ -120,6 +119,6 @@ function getFiles(dirname) {
     .readdirSync(dirname, {
       withFileTypes: true
     })
-    .filter(dirent => !dirent.isDirectory())
-    .map(dirent => dirent.name);
+    .filter(item => !item.isDirectory())
+    .map(item => item.name)
 }
